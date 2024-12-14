fec5e4c533269156202a925a8a29a7df1cece5eb
Mehmet Ali Atik <alimehmetatik@gmail.com>
2024-12-14 12:52:57 +0300

    Sound and Text Effect

--- Assets/Scripts/DiolugeManager.cs
+++ Assets/Scripts/DiolugeManager.cs
@@ -1,35 +1,40 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro; // TextMeshPro namespace'ini ekliyoruz
 using UnityEngine.UI;
 
 public class DialogManager : MonoBehaviour
 {
-    public Dialog[] allDialogs;  // Tüm diyalogları tutan dizi
-    public Dialog currentDialog; // Şu anda gösterilen diyalog
+    public Dialog[] allDialogs;
+    public Dialog currentDialog;
     public ConfidenceSystem _confidenceSystem;
     public int currentDiologQuestion;
 
-    // UI Elemanları için TextMeshProUGUI referansları
-    public TextMeshProUGUI mainText;  // Ana diyalog metni
-    public Button option1Button;      // 1. seçenek butonu
-    public Button option2Button;      // 2. seçenek butonu
-    public Button option3Button;      // 3. seçenek butonu
+    public TextMeshProUGUI mainText;
+    public Button option1Button;
+    public Button option2Button;
+    public Button option3Button;
 
-    // Seçeneklerin TextMeshProUGUI referansları
     public TextMeshProUGUI option1Text;
     public TextMeshProUGUI option2Text;
     public TextMeshProUGUI option3Text;
 
+    [SerializeField] private float typingSpeed = 0.05f; // Yazma hızı
+    [SerializeField] private AudioSource typingAudioSource; // Yazma sesi
+    [SerializeField] private AudioClip typingSound;
+
     private void Start()
     {
+        //load diyolog sıfır yerine tetikleyen olay gerçekleşince olacak 
+        
         LoadDialog("0");
         _confidenceSystem = FindObjectOfType<ConfidenceSystem>();
     }
 
     public void LoadDialog(string dialogId)
     {
-        // Dialog ID'ye göre doğru diyalogu yükle
         foreach (Dialog dialog in allDialogs)
         {
             if (dialog.dialogId == dialogId)
@@ -43,20 +48,17 @@ public class DialogManager : MonoBehaviour
 
     private void DisplayDialog(Dialog dialog)
     {
-        // Ana metni TextMeshPro ile göster
-        mainText.text = dialog.mainString;
+        StopTypingEffect(); // Önceki yazma efekti ve sesi durdur
+        StartCoroutine(TypeText(dialog.mainString)); // Yeni yazdırmayı başlat
 
-        // Seçenekleri TextMeshPro ile göster
         option1Text.text = dialog.option1;
         option2Text.text = dialog.option2;
         option3Text.text = dialog.option3;
 
-        // Butonları etkinleştir
         option1Button.gameObject.SetActive(true);
         option2Button.gameObject.SetActive(true);
         option3Button.gameObject.SetActive(true);
-        
-        // Her bir butona tıklama işlevselliği ekle
+
         option1Button.onClick.RemoveAllListeners();
         option2Button.onClick.RemoveAllListeners();
         option3Button.onClick.RemoveAllListeners();
@@ -68,12 +70,43 @@ public class DialogManager : MonoBehaviour
         currentDiologQuestion = dialog.questionId;
     }
 
+    private IEnumerator TypeText(string text)
+    {
+        mainText.text = ""; 
+
+        foreach (char letter in text.ToCharArray())
+        {
+            mainText.text += letter;
+
+            if (typingAudioSource != null && typingSound != null && !typingAudioSource.isPlaying)
+            {
+                typingAudioSource.clip = typingSound;
+                typingAudioSource.Play();
+            }
+
+            yield return new WaitForSeconds(typingSpeed);
+        }
+
+        if (typingAudioSource != null && typingAudioSource.isPlaying)
+        {
+            typingAudioSource.Stop();
+        }
+    }
+
+    private void StopTypingEffect()
+    {
+        StopAllCoroutines();
+
+        if (typingAudioSource != null && typingAudioSource.isPlaying)
+        {
+            typingAudioSource.Stop();
+        }
+    }
+
     public void OnOptionSelected(int option)
     {
-        
         string nextDialogId = null;
-        
-        // Seçilen seçeneğe göre bir sonraki diyalog ID'sini al
+
         switch (option)
         {
             case 1:
@@ -87,11 +120,9 @@ public class DialogManager : MonoBehaviour
                 break;
         }
 
-        // Yeni diyalog ID'si varsa, yeni diyalogu yükle
         if (nextDialogId != null)
         {
             LoadDialog(nextDialogId);
         }
-        
     }
 }
